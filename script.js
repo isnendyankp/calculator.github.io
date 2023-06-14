b398caab3a0628b75387be6626f82d4c2a8659de
isnendyan <isnendyankp@gmail.com>
2023-06-14 08:09:11 +0700

    S19-184: Add event for sendNumberValue

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function sendNumberValue(number) {
 
 
 console.log(inputBtns);
+
+// Add Event Listeners for numbers, operators, decimal
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } 
+});
