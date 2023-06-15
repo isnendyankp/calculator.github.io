f86189baebe82a138436c2796d4d2b6f22970f6d
isnendyan <isnendyankp@gmail.com>
2023-06-15 08:23:27 +0700

    S19-184: Add event for operator

--- script.js
+++ script.js
@@ -14,5 +14,7 @@ console.log(inputBtns);
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
-  } 
+  } else if (inputBtn.classList.contains('operator')) {
+    inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
+  }
 });
