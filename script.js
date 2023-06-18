7e22f4b3bbded44bdd813f2cb93f7e47e5b0c54a
isnendyan <isnendyankp@gmail.com>
2023-06-18 12:45:43 +0700

    S19-187: fix Calculate first and second values

--- script.js
+++ script.js
@@ -29,8 +29,16 @@ function addDecimal() {
 
 // Calculate first and second values depending on operator
 const calculate = {
-  
-}
+  '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+
+  '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+
+  '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+
+  '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+
+  '=': (firstNumber, secondNumber) => secondNumber,
+};
 
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
